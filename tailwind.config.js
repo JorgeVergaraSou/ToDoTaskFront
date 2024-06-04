dddd194d86830553baf44f09b295f0a9ff71ecd9
joaquinmancini <mancinijoaquinandres@gmail.com>
2024-06-04 00:05:09 -0300

    agrego paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,36 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Nunito', 'sans-serif']
+      },
+      colors: {
+        primary: {
+          grade1: '#BA4D10',
+          grade2: '#E96E29',
+          grade3: '#FC8F51',
+          grade4: '#FFA775',
+          grade5: '#FFC2A0'
+        },
+        secondary: {
+          grade1: '#14427A',
+          grade2: '#255A99',
+          grade3: '#3E6DA5',
+          grade4: '#5F8ABE',
+          grade5: '#8FB1D9'
+        },
+        tertiary: {
+          grade1: '#BA7810',
+          grade2: '#E99E29',
+          grade3: '#FCBA51',
+          grade4: '#FFC975',
+          grade5: '#FFDAA0'
+        },
+        cuaternary: {
+          grade1: '#0A7A5B',
+          grade2: '#1B9976',
+          grade3: '#35A586',
+          grade4: '#57BEA2',
+          grade5: '#88D9C3'
+        }
       }
     }
   },
